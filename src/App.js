b2d3d0ccd50eaedbc0657618ed5b2b0160d8096b
ElSammen <112492930+ElSammen@users.noreply.github.com>
2023-03-08 13:42:00 +0000

    LS: Pagination functionality to Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './css/App.css';
 import Navigation from './components/Navigation'
+import Home from './Pages/Home'
+import Events from './Pages/Events'
+import Contact from './Pages/Contact';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
